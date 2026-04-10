87cf9f21d71f321e4231733c1dd997f334668416
Maiq-The-Liar <tobias.odermatt@hotmail.ch>
2026-04-10 02:45:59 +0200

    fix

--- Riddles/riddles.py
+++ Riddles/riddles.py
@@ -5,6 +5,9 @@ from pathlib import Path
 import discord
 from discord import app_commands
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # =========================================================
 # CONFIG
