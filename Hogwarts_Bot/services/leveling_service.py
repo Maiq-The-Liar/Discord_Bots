570fbac14cfcca491d2934d8677ff76d7c76c9ac
Maiq-The-Liar <tobias.odermatt@hotmail.ch>
2026-04-12 00:44:14 +0200

    YEAR_MONTH_THRESHOLDS  only one year

--- Hogwarts_Bot/services/leveling_service.py
+++ Hogwarts_Bot/services/leveling_service.py
@@ -12,11 +12,11 @@ class LevelingService:
     YEAR_MONTH_THRESHOLDS: dict[int, int] = {
         1: 0,
         2: 1,
-        3: 3,
-        4: 6,
-        5: 10,
-        6: 15,
-        7: 22,
+        3: 2,
+        4: 4,
+        5: 6,
+        6: 9,
+        7: 12,
     }
 
     def __init__(self, user_repo: UserRepository):
