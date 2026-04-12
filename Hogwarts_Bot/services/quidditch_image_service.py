f137ba8a2585f15292347e766138c06d707cdf63
Maiq-The-Liar <tobias.odermatt@hotmail.ch>
2026-04-12 23:46:53 +0200

    d

--- Hogwarts_Bot/services/quidditch_image_service.py
+++ Hogwarts_Bot/services/quidditch_image_service.py
@@ -22,8 +22,8 @@ class QuidditchImageService:
     BEATER_2_Y = 780
     KEEPER_Y = 1025
 
-    SCORE_LEFT = (270, 130)
-    SCORE_RIGHT = (720, 130)
+    SCORE_LEFT = (270, 110)
+    SCORE_RIGHT = (730, 110)
 
     SIDE_TEXT_MAX_WIDTH = 470
 
