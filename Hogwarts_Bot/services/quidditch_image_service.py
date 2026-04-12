ca5aee754a056e0ebeada39c69fa75de4b986312
Maiq-The-Liar <tobias.odermatt@hotmail.ch>
2026-04-13 00:01:40 +0200

    d

--- Hogwarts_Bot/services/quidditch_image_service.py
+++ Hogwarts_Bot/services/quidditch_image_service.py
@@ -22,8 +22,8 @@ class QuidditchImageService:
     BEATER_2_Y = 780
     KEEPER_Y = 1025
 
-    SCORE_LEFT = (220, 120)
-    SCORE_RIGHT = (680, 115)
+    SCORE_LEFT = (210, 120)
+    SCORE_RIGHT = (670, 115)
 
     SIDE_TEXT_MAX_WIDTH = 470
 
@@ -63,7 +63,7 @@ class QuidditchImageService:
             left_lineup = home_lineup
             right_lineup = away_lineup
 
-        score_font = self._load_score_font(110)
+        score_font = self._load_score_font(105)
 
         self._draw_centered_text(
             draw,
